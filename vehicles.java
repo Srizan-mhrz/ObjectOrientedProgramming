458ed5e37dcc5f68b6269b6adfb28627654dece5
Srizan-mhrz <shrijan.mhrz@gmail.com>
2025-06-07 11:31:28 +0545

    homework

---
+++ vehicles.java
@@ -0,0 +1,11 @@
+class vehicles{
+    public void drive(){
+        System.out.println("broom broom");
+    }
+}
+
+class Car extends vehicles{
+    public void displayDetails(){
+        System.out.println("this is a car");
+    }
+} 
